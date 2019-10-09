3bf3c5d17e9870b25d370e2d1fb7974bfd8f48a0
MarySadness <marysadness160@gmail.com>
2019-10-10 02:01:49 +0300

    Добавлен шаблон для шифра Цезаря

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,4 +1,4 @@
-def encrypt_caesar(plaintext: str):
+def encrypt_caesar(plaintext: str) -> str:
     """
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
@@ -9,63 +9,55 @@ def encrypt_caesar(plaintext: str):
     >>> encrypt_caesar("")
     ''
     """
-    ciphertext = plaintext
-    for i in range(k):
+    ciphertext = ''
+    for i in range(len(plaintext)):
         f = plaintext[i] #символ
         h = f #симыол, который при необходимости будет изменяться
         j = ord(plaintext[i]) #номер позиции символа
-        if (j >= ord('a') and j <= ord('z')) or (j >= ord('A') and j <= ord('Z')):
-            if (j >= ord('d') and j <= ord('z')) or (j >= ord('D') and j <= ord('Z')):
-                h = chr(position - 3)
-                plaintext = plaintext.replace(f, h)
-            else:
-                if j <= ord('d'):
-                    x = ord('x')
-                    r = ord('')
-                elif j <= ord('D'):
-                    x = ord('X')
-                    r = ord('A')
-                r = position - r + x
-                h = chr(r)
-        ciphertext = plaintext.replace(f, h)
-
-
+        if (j >= ord('a') and j <= ord('w')) or (j >= ord('A') and j <= ord('W')):
+            h = chr(j + 3)
+        elif (j >= ord('x') and j <= ord('z')) or (j >= ord('X') and j <= ord('Z')):
+            if j <= ord('Z'):
+                z = ord('Z')
+                r1 = ord('A')
+            elif j <= ord('z'):
+                z = ord('z')
+                r1 = ord('a')
+            r1 = r1 + z - j
+            h = chr(r1)
+        ciphertext += h
     return ciphertext
 
 
-def encrypt_caesar(ciphertext):
-	"""
-	>>> decrypt_caesar("SBWKRQ")
-	'PYTHON'
-	>>> decrypt_caesar("sbwkrq")
-	'python'
-	>>> decrypt_caesar("Sbwkrq3.6")
-	'Python3.6'
-	>>> decrypt_caesar("")
-	''
-	"""
-	for i in range(k):
-		plaintext = ciphertext
-		position = ord(ciphertext[i]) #номер позиции символа
-		p = chr(position) #символ
-		u = p #симыол, который при необходимости будет изменяться
-	    if (ord(ciphertext[i]) >= ord('a') and ord(ciphertext[i]) <= ord('z')) or (ord(ciphertext[i]) >= ord('A') and ord(ciphertext[i]) <= ord('Z')):
-			if (ord(ciphertext[i]) >= ord('d')) or (ord(ciphertext[i]) >= ord('D')):
-			    position = ord(ciphertext[i])
-		        u = chr(position - 3)
-		        p = chr(position)
-                plaintext = ciphertext.replace(p, u)
-		else:
-			if ord(ciphertext[i]) <= ord('d'):
-			    position = ord(ciphertext[i])
-			    a = ord('a')
-			    r = ord('x') + (position - a)
-			    u = chr(r)
-            else:
-                position = ord(ciphertext[i])
-			    a = ord('A')
-			    r = ord('X') + (position - a)
-			    u = chr(r)
-		    p = chr(position)
-	    plaintext = ciphertext.replace(p, u)
-	return plaintext
+def decrypt_caesar(ciphertext: str) -> str:
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    plaintext = ""
+    for i in range(len(ciphertext)):
+        u = ciphertext[i]  # символ
+        k = u  # симыол, который при необходимости будет изменяться
+        t = ord(ciphertext[i])  # номер позиции символа
+        if (t >= ord('d') and t <= ord('z')) or (t >= ord('D') and t <= ord('Z')):
+            u = chr(t - 3)
+        elif (t >= ord('a') and t <= ord('c')) or (t >= ord('A') and t <= ord('C')):
+            if t <= ord('C'):
+                c = ord('C')
+                r2 = ord('Z')
+            elif t <= ord('c'):
+                c = ord('c')
+                r2 = ord('z')
+            r2 = r2 - (c - t)
+            u = chr(r2)
+        plaintext += u
+
+
+
+    return plaintext

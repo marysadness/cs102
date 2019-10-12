37f86d84bb06eb6700b6773e0733153a2c95b212
marysadness <56173118+marysadness@users.noreply.github.com>
2019-10-12 10:44:37 +0300

    Исправлен шифр Цезаря

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,26 +11,18 @@ def encrypt_caesar(plaintext: str) -> str:
     """
     ciphertext = ''
     for i in range(len(plaintext)):
-<<<<<<< HEAD
         h = plaintext[i]  # символ
         j = ord(plaintext[i])  # номер позиции символа
-        if (j >= ord('a') and j <= ord('x')) or (j >= ord('A') and j <= ord('X')):
-            h = chr(j + 3)
-        elif (j >= ord('y') and j <= ord('z')) or (j >= ord('Y') and j <= ord('Z')):
-=======
-        f = plaintext[i] #символ
-        h = f #симыол, который при необходимости будет изменяться
-        j = ord(plaintext[i]) #номер позиции символа
         if (j >= ord('a') and j <= ord('w')) or (j >= ord('A') and j <= ord('W')):
             h = chr(j + 3)
-        elif (j >= ord('x') and j <= ord('z')) or (j >= ord('X') and j <= ord('Z')):
->>>>>>> release-1.0
-            if j <= ord('Z'):
-                z = ord('Z')
-                r1 = ord('A')
-            elif j <= ord('z'):
-                z = ord('z')
-                r1 = ord('a')
+        elif (j >= ord('x') and j <= ord('z')):
+            z = ord('z')
+            r1 = ord('a')
+            r1 = r1 + z - j
+            h = chr(r1)
+        elif (j >= ord('X') and j <= ord('Z')):
+            z = ord('Z')
+            r1 = ord('A')
             r1 = r1 + z - j
             h = chr(r1)
         ciphertext += h
@@ -48,44 +40,26 @@ def decrypt_caesar(ciphertext: str) -> str:
     >>> decrypt_caesar("")
     ''
     """
-<<<<<<< HEAD
 
 
     plaintext = ''
     for i in range(len(ciphertext)):
         u = ciphertext[i]  # символ
-=======
-    plaintext = ""
-    for i in range(len(ciphertext)):
-        u = ciphertext[i]  # символ
-        k = u  # симыол, который при необходимости будет изменяться
->>>>>>> release-1.0
-        t = ord(ciphertext[i])  # номер позиции символа
+        t = ord(ciphertext[i])
         if (t >= ord('d') and t <= ord('z')) or (t >= ord('D') and t <= ord('Z')):
             u = chr(t - 3)
-        elif (t >= ord('a') and t <= ord('c')) or (t >= ord('A') and t <= ord('C')):
-            if t <= ord('C'):
-                c = ord('C')
-<<<<<<< HEAD
-                r2 = ord('A')
-                z = ord('Z')
-            elif t <= ord('c'):
-                c = ord('c')
-                r2 = ord('a')
-                z = ord('z')
-            r2 = z - (c - t)
-            u = chr(r2)
+        elif (t >= ord('a') and t <= ord('c')):
+            z = ord('z')
+            r1 = ord('a')
+            r1 = z -(t - r1)
+            u = chr(r1)
+        elif (t >= ord('A') and t <= ord('C')):
+            z = ord('Z')
+            r1 = ord('A')
+            r1 = z -(t - r1)
+            u = chr(r1)
         plaintext += u
-=======
-                r2 = ord('Z')
-            elif t <= ord('c'):
-                c = ord('c')
-                r2 = ord('z')
-            r2 = r2 - (c - t)
-            u = chr(r2)
-        plaintext += u
+    return plaintext
 
 
-
->>>>>>> release-1.0
     return plaintext
